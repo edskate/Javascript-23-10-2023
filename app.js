51e261e3d19fa13523ca387d6be8a6c76a519d34
edilson gomes de lima <edskateicevida@gmail.com>
2023-10-23 02:29:05 -0300

    menssagen exibida

---
+++ app.js
@@ -0,0 +1,11 @@
+// Declarando uma variável usando let
+let mensagem = "Olá, mundo!";
+
+// Exibindo o valor da variável no console
+console.log(mensagem);
+
+   // Seu código JavaScript aqui
+   let mensagem = "Olá, mundo!";
+        
+   // Exiba o resultado na página
+   document.getElementById("resultado").innerHTML = mensagem;
